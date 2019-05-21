f2ff8ab91a919e7c14472a3966a5e68090f23d2c
Jinyix <lulinihao@hotmail.com>
2019-05-21 13:59:14 +0800

    xjy

--- Frog/models.py
+++ Frog/models.py
@@ -4,15 +4,10 @@ from django.db import models
 
 class User(models.Model):
     '''用户表'''
-    genders = (
-        (0, '男'),
-        (1, '女'),
-    )
-
     email = models.ForeignKey(max_length=30,unique=True)
     name = models.CharField(max_length=10)
     password = models.CharField(max_length=64)
-    gender = models.IntegerField(max_length=32, choices=genders, default='男')
+    gender = models.IntegerField(max_length=32, default='男')
     telephone = models.IntegerField()
 
 
