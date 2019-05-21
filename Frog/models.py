a15676da43edf7df43cf5090c69611ac9746a703
Jinyix <lulinihao@hotmail.com>
2019-05-21 13:56:18 +0800

    xjy

--- Frog/models.py
+++ Frog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    '''用户表'''
+    genders = (
+        (0, '男'),
+        (1, '女'),
+    )
+
+    email = models.ForeignKey(max_length=30,unique=True)
+    name = models.CharField(max_length=10)
+    password = models.CharField(max_length=64)
+    gender = models.IntegerField(max_length=32, choices=genders, default='男')
+    telephone = models.IntegerField()
+
+
