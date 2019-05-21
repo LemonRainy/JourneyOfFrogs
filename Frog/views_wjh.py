6eb17b3339c947a1ee6074b8f0b840d2d2edcb73
16301110@bjtu.edu.cn <16301110@bjtu.edu.cn>
2019-05-21 16:22:32 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/LemonRainy/JourneyOfFrogs""
    This reverts commit f821ff64e2e224ac4238639862e27b1a2bb19099.

--- Frog/views_wjh.py
+++ Frog/views_wjh.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from Frog import models
 
@@ -13,14 +13,20 @@ def expert(request):
     # 输入的邮箱和密码
     email_input = "2973493373@qq.com"
     password = "123456"
-    lines = models.Expert.objects.filter(email=email_input, password=password)
-    if lines:
+    expert = models.Expert.objects.get(email=email_input, password=password)
+
+    if expert:
         # 登录成功返回页面
-        return render(request, '../templates/expertPage.html')
+        request.session['expert_email'] = expert.email
+        return render(request, '../templates/expertPage.html', {'expert': expert})
     else:
         return HttpResponse("用户名或者密码错误")
 
 
-
 def orderHandling(request):
-    return render(request, '../templates/orderHandlingPage.html')
+    v = request.session.get('expert_email')
+    if v:
+        orders = models.Order.objects.filter(expertEmail=v, state=0)
+        return render(request, '../templates/orderHandlingPage.html', {'orders': orders})
+    else:
+        return redirect('/expert')
