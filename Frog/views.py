df99cdb600f8b6ee6bd0df1b6aa6077d60833364
16301110@bjtu.edu.cn <16301110@bjtu.edu.cn>
2019-05-21 09:02:08 +0800

    添加模板继承

--- Frog/views.py
+++ Frog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, '../templates/index.html')
+    return render(request, '../templates/complete/indexPage.html')
 
 
 
