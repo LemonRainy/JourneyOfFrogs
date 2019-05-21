eea9214d1acfd990c1cc226e54a8a7131b34273f
Jiahang <2973493373@qq.com>
2019-05-21 10:03:40 +0800

    更新

--- Frog/views_wjh.py
+++ Frog/views_wjh.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from Frog import models
 
 
 def expertOrderList(request):
@@ -6,7 +10,17 @@ def expertOrderList(request):
 
 
 def expert(request):
-    return render(request, '../templates/expertPage.html')
+    # 输入的邮箱和密码
+    email_input = "2973493373@qq.com"
+    password = "123456"
+    lines = models.Expert.objects.filter(email=email_input, password=password)
+    if lines:
+        # 登录成功返回页面
+        return render(request, '../templates/expertPage.html')
+    else:
+        return HttpResponse("用户名或者密码错误")
+
+
 
 def orderHandling(request):
     return render(request, '../templates/orderHandlingPage.html')
